c17ce9d7d25d8fe167197fd6c66ab87da48f0e70
shimotsu4431 <shimotsu13@gmail.com>
2022-04-03 12:23:21 +0900

    fix Footer

---
+++ src/components/common/Footer/index.test.tsx
@@ -0,0 +1,10 @@
+// TODO: react18の対応
+// import '@testing-library/jest-dom'
+
+// describe('test Footer', () => {
+//   test('year が正しく表示されているか', () => {
+//     const { getByText } = render(<Footer />)
+
+//     expect(getByText('2022')).toBeInTheDocument()
+//   })
+// })
--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,11 +1,16 @@
 import { Center, Text } from '@chakra-ui/react'
 import React from 'react'
 
+// 西暦入れる、テスト書く
+
 const Footer: React.FC = () => {
+  const date = new Date()
+  const year = date.getFullYear()
+
   return (
     <footer>
       <Center p={4}>
-        <Text>Footer</Text>
+        <Text>Footer | {year}</Text>
       </Center>
     </footer>
   )
